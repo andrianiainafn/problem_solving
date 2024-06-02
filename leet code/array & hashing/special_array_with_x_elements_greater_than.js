ae91e863e10e3d1663ff366f9d887988033f7a72
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-06-02 21:57:23 +0300

    special_array_with_x_elements_greater_than resolved

---
+++ leet code/array & hashing/special_array_with_x_elements_greater_than.js
@@ -0,0 +1,25 @@
+const specialArray = function(nums) {
+    nums.sort((a,b)=> a-b)
+    let l= 0
+    let r= nums.length
+    let x= 1
+    let m =0
+    if(nums.length == 1 && nums[0] !=0 ){
+        return 1
+    }
+   while (l < r){
+       if(nums[l] >= x && x == r-l){
+           return x
+       }
+        else if(nums[l] >= x && x != r-l){
+           x++
+       }
+       if (nums[l]<x){
+           l++
+       }
+
+   }
+    return -1
+};
+
+console.log(specialArray([3,5]))
